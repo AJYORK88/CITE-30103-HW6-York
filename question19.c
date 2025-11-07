c19a190db9e06463fe7e5d70b241794b907326b8
AJYORK88 <ajyorktx@att.net>
2025-11-07 11:40:40 -0600

    Completed Question 19: While loop sum 1 to n

---
+++ question19.c
@@ -0,0 +1,19 @@
+/* Question 19: Sum numbers from 1 to n using while loop */
+
+#include <stdio.h>
+
+int main(void) {
+    int n, sum = 0, i = 1;
+    
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    while (i <= n) {
+        sum += i;
+        i++;
+    }
+    
+    printf("Sum = %d\n", sum);
+    
+    return 0;
+}
